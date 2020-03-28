8713b7edef015f0b5deb2851911da60b03e289f1
Josh Nussbaum <joshnuss@gmail.com>
2020-03-28 01:15:06 -0400

    Adds custom styling

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import * as sapper from '@sapper/app'
-import 'highlight.js/styles/github.css'
+import 'highlight.js/styles/grayscale.css'
 
 sapper.start({
   target: document.querySelector('#sapper')

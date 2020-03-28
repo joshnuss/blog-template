5be944f97bcc630fc8513b87fe76326c26e6b731
Josh Nussbaum <joshnuss@gmail.com>
2020-03-27 23:45:49 -0400

    Adds highlighting

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 import * as sapper from '@sapper/app'
+import 'highlight.js/styles/github.css'
 
 sapper.start({
   target: document.querySelector('#sapper')
--- src/routes/posts/[permalink].svelte
+++ src/routes/posts/[permalink].svelte
@@ -7,7 +7,14 @@
 </script>
 
 <script>
+  import {onMount} from 'svelte'
+  import hljs from 'highlight.js'
+
   export let post
+
+  onMount(() => {
+    hljs.initHighlighting()
+  })
 </script>
 
 <h1>{post.title}</h1>
